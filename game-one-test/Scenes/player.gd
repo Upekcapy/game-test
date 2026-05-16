a63b95307e85d9b9eb93d4b1388e885cc31eced0
drunkrabb1t <86307874+drunkrabb1t@users.noreply.github.com>
2026-05-16 16:03:58 -0400

    Basic character movement + Background
    -Two scenes
      -(1) Player (2) Level
    -Player node is in level scene
    -Level is now default scene
    -Player has basic movement script

---
+++ game-one-test/Scenes/player.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var speed = 150
+
+func _ready() -> void:
+	pass 
+
+
+func _process(delta: float) -> void:
+
+	if Input.is_action_pressed("move_right"):
+		position += Vector2(1,0) * speed * delta
+	if Input.is_action_pressed("move_left"):
+		position -= Vector2(1,0) * speed * delta
